f64a4b84af5e833415e9aece4aec9ebb16a414c9
TokinNik <tonirotok@gmail.com>
2023-03-29 15:21:38 +0700

    build 0.2 Added bloc template and screen directory

--- src/main/kotlin/com/flutter/ui/utils/flutteruiutils/CreateScreenTemplate.kt
+++ src/main/kotlin/com/flutter/ui/utils/flutteruiutils/CreateScreenTemplate.kt
@@ -1,14 +1,18 @@
 package com.flutter.ui.utils.flutteruiutils
 
 import com.intellij.ide.IdeView
+import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.ide.fileTemplates.FileTemplateUtil
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.impl.file.PsiDirectoryFactory
 import com.intellij.util.IncorrectOperationException
 import org.apache.velocity.runtime.parser.ParseException
@@ -28,18 +32,20 @@ class MyDialog(val event: AnActionEvent) : DialogWrapper(true) {
         init()
     }
 
-    lateinit var editName: JTextField
-    lateinit var editBlocName: JTextField
-    lateinit var editScreenName: JTextField
+    private val dataContext: DataContext = event.dataContext
+    private val view: IdeView? = LangDataKeys.IDE_VIEW.getData(dataContext)
+    private val project: Project? = CommonDataKeys.PROJECT.getData(dataContext)
+    private val dir: PsiDirectory? = view?.orChooseDirectory
 
+    private lateinit var editName: JTextField
+    private lateinit var editBlocName: JTextField
+    private lateinit var editScreenName: JTextField
+
+    private lateinit var config: ScreenConfig
 
     override fun doOKAction() {
-        val dataContext: DataContext = event.getDataContext()
+        if (view == null) return
 
-        val view: IdeView = LangDataKeys.IDE_VIEW.getData(dataContext) ?: return
-
-        val project = CommonDataKeys.PROJECT.getData(dataContext)
-        val dir = view.orChooseDirectory
         if (dir == null || project == null) {
             throw IllegalArgumentException("Provided directory is null")
         }
@@ -47,16 +53,7 @@ class MyDialog(val event: AnActionEvent) : DialogWrapper(true) {
         val projectPath = ProjectFileIndex.getInstance(project).getContentRootForFile(dir.virtualFile)
             ?: throw IllegalArgumentException("Unable to get project path")
 
-        val relativePath = VfsUtilCore.getRelativePath(dir.virtualFile, projectPath)
-            .orEmpty()
-
-        val newPath = "${projectPath.toNioPath()}/$relativePath"
-
-        val newDir = VfsUtil.createDirectories(newPath)
-        val newDirForReals = PsiDirectoryFactory.getInstance(project).createDirectory(newDir)
-
-
-        val template = FileTemplateManager.getInstance(dir.project).getInternalTemplate("Tmp")
+        val relativePath = VfsUtilCore.getRelativePath(dir.virtualFile, projectPath).orEmpty()
 
         var screenName = editScreenName.text
         screenName = screenName.replaceFirstChar { (screenName.first().uppercase()) }
@@ -64,20 +61,94 @@ class MyDialog(val event: AnActionEvent) : DialogWrapper(true) {
         var blocName = if (editBlocName.text.isNullOrBlank()) screenName else editBlocName.text
         blocName = blocName.replaceFirstChar { (blocName.first().uppercase()) }
 
-        val strProp = Properties()
-        strProp.setProperty("NAME1", screenName)
-        strProp.setProperty("NAME2", blocName)
+        val screenFileName = editName.text
+        val blocFileName = blocName.lowercase() + "_bloc"
+        val stateFileName = blocName.lowercase() + "_state"
+        val eventFileName = blocName.lowercase() + "_event"
 
+        config = ScreenConfig(screenFileName, screenName, blocFileName, blocName, stateFileName, eventFileName)
+
+        createScreenTemplate(projectPath, relativePath)
+        createBlocTemplate(projectPath, relativePath)
+        createStateTemplate(projectPath, relativePath)
+        createEventTemplate(projectPath, relativePath)
+    }
+
+    private fun createScreenTemplate(projectPath: VirtualFile, relativePath: String) {
+        val template = FileTemplateManager.getInstance(dir!!.project).getInternalTemplate("tmp_screen")
+
+        val screenFilePath = "${projectPath.toNioPath()}/$relativePath/${config.screenFileName}"
+        val screenFileNewDir = VfsUtil.createDirectories(screenFilePath)
+        val screenFileDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(screenFileNewDir)
+
+        val strProp = Properties()
+        strProp.setProperty("SCREEN_NAME", config.screenName)
+        strProp.setProperty("BLOC_NAME", config.blocName)
+        strProp.setProperty("SCREEN_FILE_NAME", config.screenFileName)
+        strProp.setProperty("BLOC_FILE_NAME", config.blocFileName)
+
+        createFileFromTemplate(template, config.screenFileName, strProp, screenFileDirectory)
+    }
+
+    private fun createBlocTemplate(projectPath: VirtualFile, relativePath: String) {
+        val template = FileTemplateManager.getInstance(dir!!.project).getInternalTemplate("tmp_bloc")
+
+        val blocFilePath = "${projectPath.toNioPath()}/$relativePath/${config.screenFileName}/bloc"
+        val blocFileNewDir = VfsUtil.createDirectories(blocFilePath)
+        val blocFileDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(blocFileNewDir)
+
+        val strProp = Properties()
+        strProp.setProperty("BLOC_NAME", config.blocName)
+        strProp.setProperty("BLOC_STATE_FILE_NAME", config.stateFileName)
+        strProp.setProperty("BLOC_EVENT_FILE_NAME", config.eventFileName)
+
+        createFileFromTemplate(template, config.blocFileName, strProp, blocFileDirectory)
+    }
+
+    private fun createStateTemplate(projectPath: VirtualFile, relativePath: String) {
+        val template = FileTemplateManager.getInstance(dir!!.project).getInternalTemplate("tmp_state")
+
+        val blocFilePath = "${projectPath.toNioPath()}/$relativePath/${config.screenFileName}/bloc"
+        val blocFileNewDir = VfsUtil.createDirectories(blocFilePath)
+        val blocFileDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(blocFileNewDir)
+
+        val strProp = Properties()
+        strProp.setProperty("BLOC_NAME", config.blocName)
+        strProp.setProperty("BLOC_FILE_NAME", config.blocFileName)
+
+        createFileFromTemplate(template, config.stateFileName, strProp, blocFileDirectory)
+    }
+
+    private fun createEventTemplate(projectPath: VirtualFile, relativePath: String) {
+        val template = FileTemplateManager.getInstance(dir!!.project).getInternalTemplate("tmp_event")
+
+        val blocFilePath = "${projectPath.toNioPath()}/$relativePath/${config.screenFileName}/bloc"
+        val blocFileNewDir = VfsUtil.createDirectories(blocFilePath)
+        val blocFileDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(blocFileNewDir)
+
+        val strProp = Properties()
+        strProp.setProperty("BLOC_NAME", config.blocName)
+        strProp.setProperty("BLOC_FILE_NAME", config.blocFileName)
+
+        createFileFromTemplate(template, config.eventFileName, strProp, blocFileDirectory)
+    }
+
+    private fun createFileFromTemplate(
+        template: FileTemplate,
+        screenFileName: String,
+        strProp: Properties,
+        newDirForReals: PsiDirectory,
+    ) {
         try {
             val psiFile = FileTemplateUtil.createFromTemplate(
                 template,
-                editName.text,
+                screenFileName,
                 strProp,
                 newDirForReals,
             )
                 .containingFile
             val virtualFile = psiFile.virtualFile
-            if (virtualFile != null) {
+            if (virtualFile != null && project != null) {
                 FileEditorManager.getInstance(project).openFile(virtualFile, true)
             }
         } catch (e: ParseException) {
@@ -123,3 +194,12 @@ class MyDialog(val event: AnActionEvent) : DialogWrapper(true) {
     }
 
 }
+
+data class ScreenConfig(
+    val screenFileName: String,
+    val screenName: String,
+    val blocFileName: String,
+    val blocName: String,
+    val stateFileName: String,
+    val eventFileName: String,
+)
